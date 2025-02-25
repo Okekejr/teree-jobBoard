952690ce4436d2435cbde639f7b0dffde82dee5d
Emmanuel <85338878+Okekejr@users.noreply.github.com>
2025-02-24 19:25:46 -0600

    init

---
+++ src/components/homepage.tsx
@@ -0,0 +1,11 @@
+import { Box, Heading } from "@chakra-ui/react";
+
+const Homepage = () => {
+  return (
+    <Box>
+      <Heading>Hello there</Heading>
+    </Box>
+  );
+};
+
+export default Homepage;
